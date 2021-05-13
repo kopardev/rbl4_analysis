48cfe098b985c65d762847e410daa2b12cd0e212
kopardev <vishal.koparde@nih.gov>
2021-05-13 09:39:00 -0400

    updates

--- run_HTseq_counts2DEG.sh
+++ run_HTseq_counts2DEG.sh
@@ -1,9 +1,10 @@
 g1=$1
 g2=$2
-outfile="DESeq2_${g1}-${g2}.DEG_report.html"
+outfile="docs/DESeq2_${g1}-${g2}.DEG_report.html"
 Rscript -e "library(rmarkdown);rmarkdown::render(
 input=\"HTseq_counts2DEG.Rmd\",
 output_format=\"html_document\",
 output_file=\"$outfile\",
 params=list(condition1=\"$g1\",condition2=\"$g2\"))"
+mv DESeq2_DEG_${g1}-${g2}_cpmfilter_1.txt results
 
